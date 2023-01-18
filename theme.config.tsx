7994095998928f5ffcffe9d6f26dc8c55ca25f8b
DongHeon Lee <niche3600@gmail.com>
2023-01-19 08:39:57 +0900

    theme更新

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,37 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { useRouter } from "next/router";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>My YWT</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/LeeDDHH/nextra-ywt",
   },
-  chat: {
-    link: 'https://discord.com',
+  docsRepositoryBase: "https://github.com/LeeDDHH/nextra-ywt",
+  head: () => {
+    const { asPath } = useRouter();
+    const { frontMatter } = useConfig();
+    return (
+      <>
+        <meta name="robots" content="index,follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+        <meta httpEquiv="Content-Language" content="ja" />
+        <meta name="apple-mobile-web-app-title" content="Nextra YWT" />
+        <meta name="twitter:card" content="summary_large_image" />
+        {/* <meta name="twitter:site" content="" /> */}
+        <meta property="og:url" content={`https://my-app.com${asPath}`} />
+        <meta property="og:title" content={frontMatter.title || "Nextra YWT"} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || "The next site builder"}
+        />
+      </>
+    );
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  primaryHue: 48,
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Nextra YWT Template",
   },
-}
+};
 
-export default config
+export default config;
